80823068892ca207aca12e19fc69e15b38761d90
juan <juan.orih+git@gmail.com>
2023-09-27 23:26:11 -0600

    Implementacion finalizada.

--- app/src/main/java/com/acme/restaurant/FragmentoEvento.java
+++ app/src/main/java/com/acme/restaurant/FragmentoEvento.java
@@ -4,12 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class FragmentoEvento extends Fragment {
+    /**
+     * Esta implementacion es similar a la de Ficha de Evaluacion 6 - Proyecto Fragmentos.
+     * Se agrega el evento setOnClickListener para el boton Agendar del fragmento_eventos.xml.
+     */
+    View view;
+    Button botonEventoAgendar;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragmento_eventos, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragmento_eventos, container, false);
+        botonEventoAgendar = (Button) view.findViewById(R.id.btnEventoAgendar);
+        botonEventoAgendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Agendando evento...", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 }
